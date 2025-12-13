14ddd205abf4aa918e1e42c254caa6916a7c20b5
田中瑠名 <runa@MacBook-Pro-53.local>
2025-12-13 17:45:25 +0900

    Feat: Update custom tiles to Zoom 18 and fix map center/zoom parameters

--- index.js
+++ index.js
@@ -1,29 +1,23 @@
 function initMap() {
-    var mapDiv = document.getElementById('map');
+    // 緯度・経度を18のタイルに合わせて修正
+    var initialCenter = new google.maps.LatLng(34.6853, 139.3136); 
     
-    // タイルが存在する大阪城付近の中心座標に設定
-    // 緯度: 34.68748, 経度: 135.52554 (Zoom 20 のタイル範囲の中心付近)
-    var initialCenter = new google.maps.LatLng(34.689, 135.525);
-
     var map = new google.maps.Map(document.getElementById('map'), {
-      // ズームレベルをタイルが存在する 20 に固定
-      zoom: 20, 
-      center: initialCenter,
-      mapTypeControlOptions: {
-        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'custom_map']
-      }
+        zoom: 18, // 修正: 初期ズーム
+        center: initialCenter,
+        mapTypeControlOptions: {
+            mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'custom_map']
+        }
     });
 
     var customMapType = new google.maps.ImageMapType({
-      getTileUrl: function(coord, zoom) {
-        // カスタムタイルのパス: ./custom_tiles/Zoom/X/Y.jpg
-        return './custom_tiles/' + zoom + '/' + coord.x + '/' + coord.y + '.jpg'; 
-      },
-      tileSize: new google.maps.Size(256, 256),
-      // 最大・最小ズームレベルを 20 に固定することで、カスタムタイルエリアにロックする
-      maxZoom: 20,
-      minZoom: 20, 
-      name: 'Kinoko Map'
+        getTileUrl: function(coord, zoom) {
+            return './custom_tiles/' + zoom + '/' + coord.x + '/' + coord.y + '.jpg';
+        },
+        tileSize: new google.maps.Size(256, 256),
+        maxZoom: 18, // 修正: 最大ズーム
+        minZoom: 18, // 修正: 最小ズーム
+        name: 'Kinoko Map'
     });
 
     map.mapTypes.set('custom_map', customMapType);
